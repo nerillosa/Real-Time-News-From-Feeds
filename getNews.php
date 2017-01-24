f8191f1debf0b8a2f4e1274950a90eeab717dba2
Neri Llosa <nerillosa@gmail.com>
2017-01-24 14:11:07 -0700

    Update getNews.php

--- getNews.php
+++ getNews.php
@@ -10,7 +10,7 @@
     		return;
 	}
 
-	$db = new MySqli('localhost', 'XXXXX', 'XXXXX', 'XXXXX');
+	$db = new MySqli('localhost', '*********', '*******', '********');
 
 	$result = $db->query("SELECT MAX(id) FROM news_type");
 	$row = $result->fetch_row();
