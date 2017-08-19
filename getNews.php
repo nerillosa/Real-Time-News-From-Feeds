162ba41b29c6c06ccaa9fd7674c2b4b27ed77f0f
Neri Llosa <nerillosa@gmail.com>
2017-08-19 12:53:54 -0700

    Update getNews.php

--- getNews.php
+++ getNews.php
@@ -6,11 +6,11 @@
 
 	if (filter_var($type, FILTER_VALIDATE_INT) === false) {
     		echo("type variable is not an integer");
-		$output = shell_exec("/home1/nerillos/public_html/log_error.sh $type");
+		$output = shell_exec("/home1/XXXXX/public_html/log_error.sh $type");
     		return;
 	}
 
-	$db = new MySqli('localhost', 'nerillos_neri', 'carpa1', 'nerillos_neri');
+	$db = new MySqli('localhost', 'XXXXX', 'XXXXXXX', 'XXXXXX');
 
 	$result = $db->query("SELECT MAX(id) FROM news_type");
 	$row = $result->fetch_row();
@@ -18,7 +18,7 @@
 
 	if($type > ($maxType+1) || $type < 1) {
 		echo("Non valid type");
-		$output = shell_exec("/home1/nerillos/public_html/log_error.sh $type");
+		$output = shell_exec("/home1/XXXXXX/public_html/log_error.sh $type");
 		return;
 	}
 
